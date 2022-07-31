3c31612938d56acef67424e819030e4a8fe476da
BDhanush <79576183+BDhanush@users.noreply.github.com>
2022-07-31 22:58:34 +0530

    Update main.cpp

--- Battleship cpp/main.cpp
+++ Battleship cpp/main.cpp
@@ -3,16 +3,9 @@
 
 using namespace std;
 
-#define mod 1000000007
-#define val 1000000001
 #define debug(x) cout<< #x <<"="<<x<<endl
-#define in(x) cin>>x
 #define all(x) x.begin(),x.end()
 #define PI 3.1415926535897932384626
-
-vector<char> prime();vector<int> faster_prime();
-
-long long int power();long long int powermod();
 
 int c=0;
 int bias=3;
@@ -240,71 +233,4 @@ int main()
         test();
     }
     return 0;
-}
-
-vector<char> prime(int n)                                              //O(nloglogn)
-{
-    vector<char> c(n+1,'1');
-    c.at(0)='0';
-    c.at(1)='0';
-    for(int i=2;i*i<=n;i++)
-    {
-        if(c[i]=='1')
-        {
-            for(int j=i*i;j<=n;j+=i)
-            {
-                c.at(j)='0';
-            }
-        }
-    }
-    return c;
-}
-
-vector<int> faster_prime(int n)                                         //O(n)
-{
-    vector<int> factor(n+1,0);
-    vector<int> prime;
-    for(int i=2;i<n;i++)
-    {
-        if(factor[i]==0)
-        {
-            factor.at(i)=i;
-            prime.push_back(i);
-        }
-        for(int j=0;j<prime.size() && prime[j]<=i && i*prime[j]<=n;j++)
-        {
-            factor.at(i*prime[j])=prime[j];
-        }
-    }
-    return prime;
-}
-
-long long int power(long long int a,long long int n)
-{
-    long long int pow=1;
-    for(long long int i=n;i>0;i>>=1)
-    {
-        if(i&1)
-        {
-            pow*=a;
-        }
-        a*=a;
-        //a>>=1;
-    }
-    return pow;
-}
-
-long long int powermod(long long int a,long long int n,long long int m)
-{
-    long long int pow=1;
-    a%=m;
-    for(long long int i=n;i>0;i>>=1)
-    {
-        if(i&1)
-        {
-            pow=pow*a%m;
-        }
-        a=a*a%m;
-    }
-    return pow;
 }
